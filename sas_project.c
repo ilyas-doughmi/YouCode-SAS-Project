29279c816a33232b58ef52afd24a6e1df65dc6ae
ilyas-doughmi <doughmi.projects@gmail.com>
2025-09-09 21:04:58 +0100

    ajoute | show players

---
+++ sas_project.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int id;
+    char firstname[50];
+    char lastname[50];
+    int numeroMaillot;
+    char poste[50];
+    int age;
+    int buts;
+} Player;
+
+Player players[500];
+int countPlayers = 0;
+
+void addPlayer();
+void showPlayers();
+
+int main() {
+    int choix;
+    do {
+        printf("==== MENU ====\n");
+        printf("1. Add new player\n");
+        printf("2. Show all players\n");
+        printf("0. Quit\n");
+        printf("Choose: ");
+        scanf("%d", &choix);
+
+        switch (choix) {
+            case 1: addPlayer(); break;
+            case 2: showPlayers(); break;
+            case 0: printf("Bye!\n"); break;
+            default: printf("Invalid Choice!\n"); break;
+        }
+    } while (choix != 0);
+    return 0;
+}
+
+void addPlayer() {
+    system("cls");
+    printf("=== ADDING PLAYER ===\n");
+
+    printf("Enter player first name: ");
+    scanf("%s", players[countPlayers].firstname);
+
+    printf("Enter player last name: ");
+    scanf("%s", players[countPlayers].lastname);
+
+    printf("Enter shirt number: ");
+    scanf("%d", &players[countPlayers].numeroMaillot);
+
+    printf("Enter age: ");
+    scanf("%d", &players[countPlayers].age);
+
+    printf("Enter position: ");
+    scanf("%s", players[countPlayers].poste);
+
+    printf("Enter goals: ");
+    scanf("%d", &players[countPlayers].buts);
+
+    players[countPlayers].id = countPlayers + 1;
+
+    printf("\n=== Player Added Successfully ===\n");
+    printf("ID: %d\n", players[countPlayers].id);
+    printf("Name: %s %s\n", players[countPlayers].firstname, players[countPlayers].lastname);
+    printf("Age: %d\n", players[countPlayers].age);
+    printf("Shirt Number: %d\n", players[countPlayers].numeroMaillot);
+    printf("Poste: %s\n", players[countPlayers].poste);
+    printf("Goals: %d\n", players[countPlayers].buts);
+    printf("===============================\n");
+
+    countPlayers++;
+    system("pause");
+    system("cls");
+}
+
+void showPlayers() {
+    if (countPlayers == 0) {
+        printf("No players added yet!\n");
+        return;
+    }
+
+    printf("=== LIST OF PLAYERS ===\n");
+    for (int i = 0; i < countPlayers; i++) {
+        printf("ID: %d | %s %s | Age: %d | Shirt: %d | Poste: %s | Goals: %d\n",
+               players[i].id, players[i].firstname, players[i].lastname,
+               players[i].age, players[i].numeroMaillot, players[i].poste, players[i].buts);
+    }
+    printf("========================\n");
+    system("pause");
+    system("cls");
+}
