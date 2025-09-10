3df32950fe8a1d47313f13a01c3c80c12f2eba38
ilyas-doughmi <doughmi.projects@gmail.com>
2025-09-10 12:15:13 +0100

    adding search function | seartch with id and last name

--- sas_project.c
+++ sas_project.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-typedef struct {
+typedef struct
+{
     int id;
     char firstname[50];
     char lastname[50];
@@ -23,49 +25,81 @@ void showPlayersMenu();
 void showpAlphabete();
 void triwithage();
 void triwithposte();
+void search();
+void searchwithlastname();
+
+void searchwithid();
 
 void triwithname();
 
-int main() {
+int main()
+{
     int choix;
-    do {
+    do
+    {
         printf("==== MENU ====\n");
         printf("1. Add new player\n");
         printf("2. Show all players\n");
+        printf("3. Search Player\n");
         printf("0. Quit\n");
         printf("Choose: ");
         scanf("%d", &choix);
 
-        switch (choix) {
-            case 1: addPlayerMainMenu();break;
-            case 2: showPlayersMenu(); break;
-            case 0: printf("Bye!\n");return 0; 
-            default: printf("Invalid Choice!\n"); break;
+        switch (choix)
+        {
+        case 1:
+            addPlayerMainMenu();
+            break;
+        case 2:
+            showPlayersMenu();
+            break;
+        case 3:
+            search();
+            break;
+        case 0:
+            printf("Bye!\n");
+            return 0;
+        default:
+            printf("Invalid Choice!\n");
+            break;
         }
     } while (choix != 0);
     return 0;
 }
-void addPlayerMainMenu(){
+void addPlayerMainMenu()
+{
     system("cls");
     printf("=========== ADDING PLAYER ==============\n");
     int choix;
-    do{
-    printf("1.Add 1 player\n");
-    printf("2.Add More than 1 Player\n");
-    printf("0.Go Back To MainMenu\n");
-    printf("Choose: ");
-    scanf("%d",&choix);
+    do
+    {
+        printf("1.Add 1 player\n");
+        printf("2.Add More than 1 Player\n");
+        printf("0.Go Back To MainMenu\n");
+        printf("Choose: ");
+        scanf("%d", &choix);
 
-    switch (choix){
-        case 1:addPlayer();break;
-        case 2:addmorePlayers();break;
-        case 0:system("cls");main();break;
-        default:printf("Invalid Choice!\n");break;
-    }
-        
-    }while ( choix != 0);
+        switch (choix)
+        {
+        case 1:
+            addPlayer();
+            break;
+        case 2:
+            addmorePlayers();
+            break;
+        case 0:
+            system("cls");
+            main();
+            break;
+        default:
+            printf("Invalid Choice!\n");
+            break;
+        }
+
+    } while (choix != 0);
 }
-void addPlayer() {
+void addPlayer()
+{
     system("cls");
     printf("=== ADDING PLAYER ===\n");
 
@@ -104,36 +138,50 @@ void addPlayer() {
     main();
 }
 
-void showPlayersMenu(){
+void showPlayersMenu()
+{
     int choix;
-
-    do{
+    do
+    {
         printf("========== SHOW PLAYERS ============\n");
         printf("1.in Alphabete order\n");
         printf("2.in Players AGE\n");
         printf("3.Show Players with Poste\n");
         printf("4.Back to Main Menu\n");
         printf("Choose : ");
-        scanf("%d",&choix);
+        scanf("%d", &choix);
 
-        switch(choix){
-            case 1:triwithname();break;
-            case 2:triwithage();break;
-            case 3:triwithposte();break;
-            case 4:main();break;
-            default:printf("invalid Choice\n");
+        switch (choix)
+        {
+        case 1:
+            triwithname();
+            break;
+        case 2:
+            triwithage();
+            break;
+        case 3:
+            triwithposte();
+            break;
+        case 4:
+            main();
+            break;
+        default:
+            printf("invalid Choice\n");
         }
-    }while(choix != 0);
+    } while (choix != 0);
 }
-void showPlayers() {
-    
-    if (countPlayers == 0) {
+void showPlayers()
+{
+
+    if (countPlayers == 0)
+    {
         printf("No players added yet!\n");
         return;
     }
 
     printf("=== LIST OF PLAYERS ===\n");
-    for (int i = 0; i < countPlayers; i++) {
+    for (int i = 0; i < countPlayers; i++)
+    {
         printf("ID: %d | %s %s | Age: %d | Shirt: %d | Poste: %s | Goals: %d\n",
                players[i].id, players[i].firstname, players[i].lastname,
                players[i].age, players[i].numeroMaillot, players[i].poste, players[i].buts);
@@ -143,100 +191,67 @@ void showPlayers() {
     system("cls");
 }
 
-
-void addmorePlayers(){
+void addmorePlayers()
+{
     int times;
     printf("How many players you want to enter : ");
-    scanf("%d",&times);
-    for(int i = 0 ; i < times ; i++){
+    scanf("%d", &times);
+    for (int i = 0; i < times; i++)
+    {
         system("cls");
-    printf("=== ADDING PLAYER ===\n");
+        printf("=== ADDING PLAYER ===\n");
 
-    printf("Enter player first name: ");
-    scanf("%s", players[countPlayers].firstname);
+        printf("Enter player first name: ");
+        scanf("%s", players[countPlayers].firstname);
 
-    printf("Enter player last name: ");
-    scanf("%s", players[countPlayers].lastname);
+        printf("Enter player last name: ");
+        scanf("%s", players[countPlayers].lastname);
 
-    printf("Enter shirt number: ");
-    scanf("%d", &players[countPlayers].numeroMaillot);
+        printf("Enter shirt number: ");
+        scanf("%d", &players[countPlayers].numeroMaillot);
 
-    printf("Enter age: ");
-    scanf("%d", &players[countPlayers].age);
+        printf("Enter age: ");
+        scanf("%d", &players[countPlayers].age);
 
-    printf("Enter position: ");
-    scanf("%s", players[countPlayers].poste);
+        printf("Enter position: ");
+        scanf("%s", players[countPlayers].poste);
 
-    printf("Enter goals: ");
-    scanf("%d", &players[countPlayers].buts);
+        printf("Enter goals: ");
+        scanf("%d", &players[countPlayers].buts);
 
-    players[countPlayers].id = countPlayers + 1;
+        players[countPlayers].id = countPlayers + 1;
 
-    printf("\n=== Player Added Successfully ===\n");
-    printf("ID: %d\n", players[countPlayers].id);
-    printf("Name: %s %s\n", players[countPlayers].firstname, players[countPlayers].lastname);
-    printf("Age: %d\n", players[countPlayers].age);
-    printf("Shirt Number: %d\n", players[countPlayers].numeroMaillot);
-    printf("Poste: %s\n", players[countPlayers].poste);
-    printf("Goals: %d\n", players[countPlayers].buts);
-    printf("===============================\n");
+        printf("\n=== Player Added Successfully ===\n");
+        printf("ID: %d\n", players[countPlayers].id);
+        printf("Name: %s %s\n", players[countPlayers].firstname, players[countPlayers].lastname);
+        printf("Age: %d\n", players[countPlayers].age);
+        printf("Shirt Number: %d\n", players[countPlayers].numeroMaillot);
+        printf("Poste: %s\n", players[countPlayers].poste);
+        printf("Goals: %d\n", players[countPlayers].buts);
+        printf("===============================\n");
 
-    countPlayers++;
-    system("pause");
-    system("cls");
+        countPlayers++;
+        system("pause");
+        system("cls");
     }
     main();
 }
 
-void triwithage(){
-    if(countPlayers == 0){
+void triwithage()
+{
+    if (countPlayers == 0)
+    {
         printf("No Player\n");
         return;
     }
 
-    for(int i = 0 ; i < countPlayers-1 ; i++){
-        for(int j = i + 1; j < countPlayers ; j++){
-            if(players[i].age > players[j].age){
-                Player temp = players[i];   
-                players[i] = players[j];
-                players[j] = temp;
-            }
-        }
-    }
-
-    showPlayers();  
-}
-
-
-void triwithname(){
-    if(countPlayers == 0){
-        printf("No Player\n");
-        return;
-    }
-
-    for(int i = 0 ; i < countPlayers-1 ; i++){
-        for(int j = i + 1; j < countPlayers ; j++){
-            if(strcmp(players[i].lastname, players[j].lastname) > 0){
-                Player temp = players[i];   
-                players[i] = players[j];
-                players[j] = temp;
-            }
-        }
-    }
-
-    showPlayers();  
-}
-
-void triwithposte(){
-    if(countPlayers == 0){
-        printf("No Player\n");
-        return;
-    }
-
-        for(int i = 0 ; i < countPlayers-1 ; i++){
-        for(int j = i + 1; j < countPlayers ; j++){
-            if(strcmp(players[i].poste, players[j].poste) > 0){
-                Player temp = players[i];   
+    for (int i = 0; i < countPlayers - 1; i++)
+    {
+        for (int j = i + 1; j < countPlayers; j++)
+        {
+            if (players[i].age > players[j].age)
+            {
+                Player temp = players[i];
                 players[i] = players[j];
                 players[j] = temp;
             }
@@ -244,4 +259,132 @@ void triwithposte(){
     }
 
     showPlayers();
+}
+
+void triwithname()
+{
+    if (countPlayers == 0)
+    {
+        printf("No Player\n");
+        return;
+    }
+
+    for (int i = 0; i < countPlayers - 1; i++)
+    {
+        for (int j = i + 1; j < countPlayers; j++)
+        {
+            if (strcmp(players[i].lastname, players[j].lastname) > 0)
+            {
+                Player temp = players[i];
+                players[i] = players[j];
+                players[j] = temp;
+            }
+        }
+    }
+
+    showPlayers();
+}
+
+void triwithposte()
+{
+    if (countPlayers == 0)
+    {
+        printf("No Player\n");
+        return;
+    }
+    for (int i = 0; i < countPlayers - 1; i++)
+    {
+        for (int j = i + 1; j < countPlayers; j++)
+        {
+            if (strcmp(players[i].poste, players[j].poste) > 0)
+            {
+                Player temp = players[i];
+                players[i] = players[j];
+                players[j] = temp;
+            }
+        }
+    }
+
+    showPlayers();
+}
+
+void search()
+{
+    int choix;
+    printf("1. Search with ID\n");
+    printf("2. Search with lastname\n");
+    printf("Choose : ");
+    scanf("%d", &choix);
+    switch (choix)
+    {
+    case 1:
+        searchwithid();
+        break;
+    case 2:
+        searchwithlastname();
+        break;
+    default:
+        printf("Invalid Choice\n");
+        break;
+    }
+}
+
+void searchwithid()
+{
+    int idsearch;
+    if (countPlayers == 0)
+    {
+        printf("No Player\n");
+        return;
+    }
+    printf("Give me ID: ");
+    scanf(" %d", &idsearch);
+    bool found = false;
+
+    for (int i = 0; i < countPlayers; i++)
+    {
+        if (idsearch == players[i].id)
+        {
+            printf("Found It\n");
+            printf("ID: %d | %s %s | Age: %d | Shirt: %d | Poste: %s | Goals: %d\n",
+                   players[i].id, players[i].firstname, players[i].lastname,
+                   players[i].age, players[i].numeroMaillot, players[i].poste, players[i].buts);
+            found = true;
+        }
+    }
+
+    if (!found)
+    {
+        printf("Not Found\n");
+    }
+}
+
+void searchwithlastname()
+{
+    int idsearch;
+    bool found = false;
+    if (countPlayers == 0)
+    {
+        printf("No Player\n");
+        return;
+    }
+    char namesearch[500];
+    printf("Enter a Last name of player you want to search for : ");
+    scanf("%s", namesearch);
+
+    for (int i = 0; i < countPlayers; i++)
+    {
+        if (strcmp(namesearch, players[i].lastname) == 0)
+        {
+            printf("Found it\n");
+            printf("ID: %d | %s %s | Age: %d | Shirt: %d | Poste: %s | Goals: %d\n",
+                   players[i].id, players[i].firstname, players[i].lastname,
+                   players[i].age, players[i].numeroMaillot, players[i].poste, players[i].buts);
+            found = true;
+        }
+    }
+    if (!found)
+    {
+        printf("Not Found\n");
+    }
 }
